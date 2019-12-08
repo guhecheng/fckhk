8ad41c5cc2b0b12359c0ff356bbebde5751c2d8e
guhecheng <guhecheng@kuaidihelp.com>
2019-12-08 23:35:15 +0800

    修改

--- route.go
+++ route.go
@@ -13,10 +13,9 @@ import (
 func main()  {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
-	router.GET("/index", func(c *gin.Context) {
-		fmt.Println("hello world")
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Posts",
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK,"index.tmpl", gin.H{
+			"title": "FckHk",
 		})
 	})
 
